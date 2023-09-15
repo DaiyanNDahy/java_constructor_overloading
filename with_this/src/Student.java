ab1cd90ae5e1c4ee1559492e46570c97324d76cc
DaiyanNDahy <daiyanndahy@gmail.com>
2023-09-15 17:48:44 +0600

    uploading data

---
+++ with_this/src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    
+    private int id,age;
+    private String name;
+
+    public Student(){
+
+    }
+
+    public Student(int id){
+        this.id=id;
+    }
+
+    public Student(int id, String name, int age){
+
+        this.id=id;
+
+        this.name=name;
+
+        this.age=age;
+    }
+}
